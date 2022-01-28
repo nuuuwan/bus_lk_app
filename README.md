6fcdfa317c2006a493f346e38837ce1edeb29b04
nuuuwan <nuuuwan@gmail.com>
2022-01-28 19:44:12 +0530

    [main] Removed tests (2022-01-28-1944)

--- README.md
+++ README.md
@@ -11,3 +11,4 @@
   * Updated package.json browserslist
   *  [2021-09-25 08:38AM] Changed Browser to Firefox; Removed failing tests
   *  [2022-01-28 07:31PM] Initial files
+  *  [2022-01-28 07:44PM] Removed tests
--- src/stateful/pages/HomePage.js
+++ src/stateful/pages/HomePage.js
@@ -1,10 +1,31 @@
 import { Component } from "react";
 
 export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {latLng:undefined};
+  }
+
+  onGetCurrentPosition(position) {
+  const latLng = [position.coords.latitude, position.coords.longitude];
+  this.setState({ latLng });
+}
+
+
+  async componentDidMount() {
+  navigator.geolocation.getCurrentPosition(
+    this.onGetCurrentPosition.bind(this)
+  );
+  }
   render() {
+    const {latLng} = this.state;
+    if (!latLng) {
+      return 'Loading...'
+    }
     return (
       <div>
-        <div>HomePage</div>
+        <h1>Bus App</h1>
+        <div>{latLng}</div>
       </div>
     )
   }
